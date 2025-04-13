4250a3ea5e98dd3f5bd1b8d900198824ebcc70a0
Fatim-coding <timabadjie12@gmail.com>
2025-04-13 15:26:40 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# print a sentence
+print("hello Everyone")
+
+# print your favourite teacher in codingal
+print("Elijah guchu")
+
+# print your teacher's country and his birthday
+print("his country is kenya his birthday is on june 26")
+
+# print his favourite colour
+print("blue")
+
+# print what do your teacher do for living
+print("he is a condingal teacher")
+  
+# print  about something nice about him when he is teaachering
+print("he have patience when he is teaching he will make sure when you don't understand he explain the work again")
